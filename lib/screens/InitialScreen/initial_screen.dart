1558f6fc314f6c8601a9f0bef473eb74391c50ba
kaloca <mega.kaloca@gmail.com>
2022-05-05 15:56:28 -0300

    finished initial screen

--- lib/screens/InitialScreen/initial_screen.dart
+++ lib/screens/InitialScreen/initial_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({Key? key}) : super(key: key);
@@ -10,10 +11,67 @@ class InitialScreen extends StatefulWidget {
 class _InitialScreenState extends State<InitialScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ElevatedButton(
-        onPressed: () => Navigator.pushNamed(context, '/scan'),
-        child: Text('Go Forward'),
+    return Scaffold(
+      backgroundColor: NeumorphicColors.background,
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.3,
+            child: Container(
+              margin: const EdgeInsets.only(top: 470),
+              alignment: Alignment.topCenter,
+              child: Image.asset(
+                'assets/images/bux-logo.png',
+                width: 1,
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // Image.network(
+                //   'https://buxbank.com.br/wp-content/uploads/2022/03/bux-logo-main.webp',
+                //   height: 90,
+                // ),
+                Container(
+                  margin: const EdgeInsets.only(top: 280, bottom: 50),
+                  child: NeumorphicText(
+                    "BuxSplit",
+                    textStyle: NeumorphicTextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    style: const NeumorphicStyle(
+                      depth: 3,
+                      intensity: 0.8,
+                      color: Color.fromARGB(255, 94, 93, 213),
+                    ),
+                  ),
+                ),
+                NeumorphicButton(
+                  style: const NeumorphicStyle(
+                    depth: 3,
+                    intensity: 0.8,
+                    color: NeumorphicColors.background,
+                  ),
+                  onPressed: () => Navigator.pushNamed(context, '/scan'),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 30,
+                    width: 80,
+                    child: const Text('Começar',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
